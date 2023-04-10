9251f1c7b849ff84c12a1f3dce091d581e57ddfd
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-10 08:25:06 -0700

    Add Basic shits

---
+++ CustomerMoghimiHome/Shared/Basic/classes/SortByEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomerMoghimiHome.Shared.Basic.Classes;
+public enum SortByEnum
+{
+    CreationDate = 1,
+    CreationDateDescending = 2,
+}
+
+public enum UserSortBy
+{
+    CreationDate = 1,
+    CreationDateDescending = 2,
+    LastLoginDate,
+    LastLoginDateDescending,
+}
--- CustomerMoghimiHome/Shared/EntityFramework/Entities/Shop/ProductCategoryEntity.cs
+++ CustomerMoghimiHome/Shared/EntityFramework/Entities/Shop/ProductCategoryEntity.cs
@@ -1,7 +1,7 @@
 ﻿using CustomerMoghimiHome.Shared.EntityFramework.Common;
 
 namespace CustomerMoghimiHome.Shared.EntityFramework.Entities.Shop;
-public class ProductCategoryEntity: BaseEntity
+public class ProductCategoryEntity : BaseEntity
 {
     public string CategoryName { get; set; } = string.Empty;
     public string CategoryDescription { get; set; } = string.Empty;
