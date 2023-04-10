5a1ee85749de8559ce4710094222ca3396aa64b7
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-10 16:02:43 -0700

    p1

---
+++ CustomerMoghimiHome/Server/EntityFramework/Extensions/Shop/ProductCategoryQueryableExtension.cs
@@ -0,0 +1,29 @@
+﻿using CustomerMoghimiHome.Server.EntityFramework.Entities.Shop;
+using CustomerMoghimiHome.Shared.Basic.Classes;
+
+namespace CustomerMoghimiHome.Server.EntityFramework.Extensions.Shop;
+
+public static class ProductCategoryEntityQueryableExtension
+{
+    public static IQueryable<ProductCategoryEntity> ApplyFilter(this IQueryable<ProductCategoryEntity> query, DefaultPaginationFilter filter)
+    {
+        
+        if (!string.IsNullOrEmpty(filter.StringValue))
+            query = query.Where(x => x.CategoryName.ToLower().Contains(filter.StringValue.ToLower().Trim()));
+
+        if (!string.IsNullOrEmpty(filter.StringValue))
+            query = query.Where(x => x.CategoryDescription.ToLower().Contains(filter.StringValue.ToLower().Trim()));
+
+        return query;
+    }
+
+    public static IQueryable<ProductCategoryEntity> ApplySort(this IQueryable<ProductCategoryEntity> query, SortByEnum? sortBy)
+    {
+        return sortBy switch
+        {
+            SortByEnum.CreationDate => query.OrderBy(x => x.CreateDate),
+            SortByEnum.CreationDateDescending => query.OrderByDescending(x => x.CreateDate),
+            _ => query.OrderByDescending(x => x.Id)
+        };
+    }
+}
---
+++ CustomerMoghimiHome/Server/EntityFramework/Repositories/Shop/IProductCategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using CustomerMoghimiHome.Server.Basic.Classes;
+using CustomerMoghimiHome.Server.EntityFramework.Common;
+using CustomerMoghimiHome.Server.EntityFramework.Entities.Shop;
+using CustomerMoghimiHome.Server.EntityFramework.Extensions.Shop;
+using CustomerMoghimiHome.Shared.Basic.Classes;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerMoghimiHome.Server.EntityFramework.Repositories.Shop;
+
+public interface IProductCategoryRepository : IRepository<ProductCategoryEntity>
+{
+    Task<ProductCategoryEntity> GetProductCategoryByIdAsync(long id);
+    Task<PaginatedList<ProductCategoryEntity>> GetProductCategoriesByFilterAsync(DefaultPaginationFilter filter);
+    Task<List<ProductCategoryEntity>> GetProductCategoriesAsync();
+}
+
+
+public class ProductCategoryRepository : Repository<ProductCategoryEntity>, IProductCategoryRepository
+{
+    private readonly IQueryable<ProductCategoryEntity> _queryable;
+
+    public ProductCategoryRepository(DataContext context) : base(context)
+    {
+        _queryable = DbContext.Set<ProductCategoryEntity>();
+    }
+
+    public async Task<ProductCategoryEntity> GetProductCategoryByIdAsync(long id) =>
+         await _queryable.SingleOrDefaultAsync(x => x.Id == id) ?? throw new NullReferenceException();
+
+    public async Task<List<ProductCategoryEntity>> GetProductCategoriesAsync() => await _queryable.ToListAsync();
+
+    public async Task<PaginatedList<ProductCategoryEntity>> GetProductCategoriesByFilterAsync(DefaultPaginationFilter filter)
+    {
+        var query = _queryable.AsNoTracking().ApplyFilter(filter).ApplySort(filter.SortBy);
+        var dataTotalCount = _queryable.Count();
+
+        return new PaginatedList<ProductCategoryEntity>()
+        {
+            Data = await query.Paginate(filter.Page, filter.PageSize).ToListAsync(),
+            TotalCount = dataTotalCount,
+            TotalPages = (int)Math.Ceiling((decimal)dataTotalCount / (decimal)filter.PageSize),
+            Page = filter.Page,
+            PageSize = filter.PageSize
+        };
+    }
+}
