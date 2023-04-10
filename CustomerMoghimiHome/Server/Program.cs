11a53c80baf2110bbaa83ebb584162ad1de7a677
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-10 15:10:51 -0700

    first migmig

--- CustomerMoghimiHome/Server/Program.cs
+++ CustomerMoghimiHome/Server/Program.cs
@@ -1,10 +1,17 @@
+using CustomerMoghimiHome.Server.EntityFramework.Common;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration
+        .GetConnectionString("MoghimiConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
