a9e6e2239c5e170046338c443827e25d84534555
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-10 07:53:20 -0700

    add entities

--- CustomerMoghimiHome/Shared/EntityFramework/Entities/Shop/ProductCategoryEntity.cs
+++ CustomerMoghimiHome/Shared/EntityFramework/Entities/Shop/ProductCategoryEntity.cs
@@ -1,4 +1,10 @@
-﻿namespace CustomerMoghimiHome.Shared.EntityFramework.Entities.Shop;
-internal class ProductCategory
+﻿using CustomerMoghimiHome.Shared.EntityFramework.Common;
+
+namespace CustomerMoghimiHome.Shared.EntityFramework.Entities.Shop;
+public class ProductCategoryEntity: BaseEntity
 {
+    public string CategoryName { get; set; } = string.Empty;
+    public string CategoryDescription { get; set; } = string.Empty;
+
+    public List<ProductEntity> ProductEntities { get; set; } = new();
 }
--- CustomerMoghimiHome/Shared/EntityFramework/Entities/Shop/ProductEntity.cs
+++ CustomerMoghimiHome/Shared/EntityFramework/Entities/Shop/ProductEntity.cs
@@ -1,4 +1,33 @@
-﻿namespace CustomerMoghimiHome.Shared.EntityFramework.Entities.Shop;
-internal class ProductEntity
+﻿using CustomerMoghimiHome.Shared.EntityFramework.Common;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerMoghimiHome.Shared.EntityFramework.Entities.Shop;
+public class ProductEntity: BaseEntity
 {
+    public string ProductName { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string BuilderCompany { get; set; } = "Microlab";
+    public string ProductDescription { get; set; } = string.Empty;
+
+    public long ProductCategoryEnityId { get; set; }
+    public ProductCategoryEntity ProductCategory { get; set; } 
+}
+
+public class vConfiguration : IEntityTypeConfiguration<ProductEntity>
+{
+    public void Configure(EntityTypeBuilder<ProductEntity> builder)
+    {
+        #region Properties features
+
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.ProductName).IsRequired();
+        builder.Property(e => e.Price).IsRequired();
+        builder.Property(e => e.ProductDescription).IsRequired();
+        builder.Property(e => e.BuilderCompany).IsRequired();
+        #endregion
+
+        builder.HasOne(x => x.ProductCategory).WithMany(x => x.ProductEntities)
+            .HasForeignKey(x => x.ProductCategoryEnityId).OnDelete(DeleteBehavior.Cascade);
+    }
 }
