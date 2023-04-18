4ea693f0abd25ed70169775f26ee42f014e9cb8c
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-18 14:36:58 -0700

    seo alt tag ui done

---
+++ CustomerMoghimiHome/Client/Pages/AdminPages/Seo/EditAlt.razor.cs
@@ -0,0 +1,35 @@
+﻿using CustomerMoghimiHome.Shared.Basic.Classes;
+using CustomerMoghimiHome.Shared.EntityFramework.DTO.Seo;
+using CustomerMoghimiHome.Shared.EntityFramework.DTO.Shop;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Net;
+
+namespace CustomerMoghimiHome.Client.Pages.AdminPages.Seo;
+
+public partial class EditAlt
+{
+    #region Pre-Load
+    [Parameter] public string Id { get; set; }
+    AltDto model = new();
+    protected override async Task OnParametersSetAsync()
+    {
+        model = await _httpService.GetValue<AltDto>(SeoRoutes.Alt + CRUDRouts.ReadOneById + $"/{Id}");
+    }
+    #endregion
+
+    #region Update
+    public async Task Update()
+    {
+        using var response = await _httpService.PutValue(SeoRoutes.Alt + CRUDRouts.Update, model);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            _snackbar.Add("Operation Done Succesfully", Severity.Success);
+        }
+        else
+        {
+            _snackbar.Add("Operation Failed", Severity.Error);
+        }
+    }
+    #endregion
+}
