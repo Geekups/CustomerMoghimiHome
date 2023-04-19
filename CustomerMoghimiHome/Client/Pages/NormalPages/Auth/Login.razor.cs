369b855e8f28e7b3a484875f03503d358ae2088e
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-19 08:35:47 -0700

    add Auth Pages

---
+++ CustomerMoghimiHome/Client/Pages/NormalPages/Auth/Login.razor.cs
@@ -0,0 +1,44 @@
+﻿using MudBlazor;
+
+namespace CustomerMoghimiHome.Client.Pages.NormalPages.Auth
+{
+    public partial class Login
+    {
+        private LoginModelDto loginModelDto = new LoginModelDto();
+
+        public bool ShowAuthError { get; set; }
+        public string? Error { get; set; }
+        bool PasswordVisibility;
+        InputType PasswordInput = InputType.Password;
+        string PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+        public async Task ExecuteLogin()
+        {
+            ShowAuthError = false;
+            var result = await _authenticationService.Login(loginModelDto);
+            if (!result.IsAuthSuccessful)
+            {
+                _snackbar.Add("Authentication Error! Please Fill Inputs Correctly", Severity.Error);
+            }
+            else
+            {
+                _snackbar.Add("Wellcome, you are navigating to home page", Severity.Success);
+                NavigationManager.NavigateTo("/");
+            }
+        }
+        void TogglePasswordVisibility()
+        {
+            if (PasswordVisibility)
+            {
+                PasswordVisibility = false;
+                PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+                PasswordInput = InputType.Password;
+            }
+            else
+            {
+                PasswordVisibility = true;
+                PasswordInputIcon = Icons.Material.Filled.Visibility;
+                PasswordInput = InputType.Text;
+            }
+        }
+    }
+}
