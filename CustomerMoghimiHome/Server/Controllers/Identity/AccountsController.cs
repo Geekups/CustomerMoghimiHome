c7847e6fca453c087aa732d907a995294318efce
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-19 09:38:59 -0700

    .

---
+++ CustomerMoghimiHome/Server/Controllers/Identity/AccountsController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using CustomerMoghimiHome.Shared.EntityFramework.DTO.DtosRelatedIdentity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace CustomerMoghimiHome.Server.Controllers.Identity
+{
+    [Route("AccountHandelMethods")]
+    [ApiController]
+
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IMapper _mapper;
+        public AccountsController(UserManager<IdentityUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        [Route("Registration")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            if (userForRegistration == null || !ModelState.IsValid)
+                return BadRequest();
+            var user = new IdentityUser
+            {
+                UserName = userForRegistration.UserName,
+                Email = userForRegistration.Email,
+                PhoneNumber = userForRegistration.PhoneNumber
+            };
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new RegistrationResponseDto { Errors = errors });
+            }
+            //await _userManager.AddToRoleAsync(user, "Viewer");
+            return StatusCode(201);
+        }
+
+        //[HttpGet]
+        //[Route("GetAllSiteUsers")]
+        //public async Task<IActionResult> GetUsers([FromQuery] PagingParameters pagingParameters)
+        //{
+        //    var userDetailDtoList = _mapper.Map<List<UserDetailDto>>(await _userManager.Users.AsNoTracking().ToListAsync());
+        //    var pagedData = PagedList<UserDetailDto>.ToPagedList(userDetailDtoList, pagingParameters.PageNumber, pagingParameters.PageSize);
+        //    Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(pagedData.MetaData));
+        //    return Ok(pagedData);
+        //}
+    }
+}
---
+++ CustomerMoghimiHome/Server/Controllers/Identity/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace CustomerMoghimiHome.Server.Controllers.Identity
+{
+    [Route("LoginHandler")]
+    [ApiController]
+
+    public class LoginController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ITokenExtension _tokenExtension;
+        public LoginController(UserManager<IdentityUser> userManager, ITokenExtension tokenExtension)
+        {
+            _userManager = userManager;
+            _tokenExtension = tokenExtension;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModelDto loginModelDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginModelDto.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginModelDto.Password))
+                return Unauthorized(new LoginResultDto { Error = "Invalid Authentication" });
+            var signingCredentials = _tokenExtension.GetSigningCredentials();
+            var claims = await _tokenExtension.GetClaimsAsync(user);
+            var tokenOptions = _tokenExtension.GenerateTokenOptions(signingCredentials, claims);
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return Ok(new LoginResultDto { IsAuthSuccessful = true, Token = token });
+        }
+    }
+}
