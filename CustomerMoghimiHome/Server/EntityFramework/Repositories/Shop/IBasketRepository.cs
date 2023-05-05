51db2b16c8b5c30004ce6c3af17f9c7eb8b8e2ff
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-05 02:33:56 -0700

    repo p1

---
+++ CustomerMoghimiHome/Server/EntityFramework/Repositories/Shop/IBasketRepository.cs
@@ -0,0 +1,26 @@
+﻿using CustomerMoghimiHome.Server.EntityFramework.Common;
+using CustomerMoghimiHome.Server.EntityFramework.Entities.Shop;
+using CustomerMoghimiHome.Shared.Basic.Classes;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerMoghimiHome.Server.EntityFramework.Repositories.Shop;
+
+public interface IBasketRepository : IRepository<BasketEntity>
+{
+    Task<BasketEntity> GetByUserIdAsync(string id);
+}
+
+
+public class BasketRepository : Repository<BasketEntity>, IBasketRepository
+{
+    private readonly IQueryable<BasketEntity> _queryable;
+
+    public BasketRepository(DataContext context) : base(context)
+    {
+        _queryable = DbContext.Set<BasketEntity>();
+    }
+
+    public async Task<BasketEntity> GetByUserIdAsync(string id) =>
+         await _queryable.SingleOrDefaultAsync(x => x.UserId == id) ?? throw new NullReferenceException();
+
+}
