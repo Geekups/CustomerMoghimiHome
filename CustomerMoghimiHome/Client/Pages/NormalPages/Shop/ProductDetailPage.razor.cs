1bd9b0fbe81f7da9aa14ee18b2edc20877bf96cd
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-05 12:17:38 -0700

    cleaning

--- CustomerMoghimiHome/Client/Pages/NormalPages/Shop/ProductDetailPage.razor.cs
+++ CustomerMoghimiHome/Client/Pages/NormalPages/Shop/ProductDetailPage.razor.cs
@@ -1,6 +1,8 @@
 ﻿using CustomerMoghimiHome.Shared.Basic.Classes;
+using CustomerMoghimiHome.Shared.Basic.Services;
 using CustomerMoghimiHome.Shared.EntityFramework.DTO.Shop;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace CustomerMoghimiHome.Client.Pages.NormalPages.Shop;
 
@@ -8,12 +10,31 @@ public partial class ProductDetailPage
 {
     #region Pre-Load
     ProductDto model = new();
-
+    string userName = "";
     [Parameter] public string Id { get; set; }
     protected override async Task OnParametersSetAsync()
     {
         model = await _httpService.GetValue<ProductDto>(ShopRoutes.Product + CRUDRouts.ReadOneById + $"/{Id}");
     }
+    protected async override Task OnInitializedAsync()
+    {
+        var authstate = await _apiAuthenticationStateProvider.GetAuthenticationStateAsync();
+        userName = authstate.User.Identity.Name ?? "";
+    }
     #endregion
 
+
+    #region Actions
+
+    public async Task AddToBasket()
+    {
+        BasketDto basket = new BasketDto()
+        {
+            UserName = userName,
+            ProductId = model.Id
+        };
+        await _httpService.PostValue(BasketRoutes.Basket + CRUDRouts.Create, basket);
+    }
+
+    #endregion
 }
--- CustomerMoghimiHome/Server/EntityFramework/Entities/Shop/BasketEntity.cs
+++ CustomerMoghimiHome/Server/EntityFramework/Entities/Shop/BasketEntity.cs
@@ -5,6 +5,7 @@ namespace CustomerMoghimiHome.Server.EntityFramework.Entities.Shop;
 public class BasketEntity : BaseEntity
 {
     public string UserId { get; set; }
+    public string UserName { get; set; }
     public int Quantity { get; set; }
     public long ProductId { get; set; }
 }
