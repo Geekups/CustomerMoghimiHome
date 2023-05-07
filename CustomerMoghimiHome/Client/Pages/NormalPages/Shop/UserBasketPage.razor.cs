b26fce683bd38ffc1f98f7d6dbbff7a56e6fd1c1
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-07 10:50:14 -0700

    first error killed

--- CustomerMoghimiHome/Client/Pages/NormalPages/Shop/UserBasketPage.razor.cs
+++ CustomerMoghimiHome/Client/Pages/NormalPages/Shop/UserBasketPage.razor.cs
@@ -1,23 +1,17 @@
 ﻿using CustomerMoghimiHome.Shared.Basic.Classes;
 using CustomerMoghimiHome.Shared.EntityFramework.DTO.Shop;
+using Microsoft.AspNetCore.Components;
 
 namespace CustomerMoghimiHome.Client.Pages.NormalPages.Shop;
 
 public partial class UserBasketPage
 {
     List<BasketDto> model = new();
-    string userName = "";
+    [Parameter]
+    public string userName { get; set; }
 
     protected async override Task OnParametersSetAsync()
     {
-        var authstate = await _apiAuthenticationStateProvider.GetAuthenticationStateAsync();
-        userName = authstate.User.Identity.Name ?? "";
-        model = await _httpService.GetValueList<BasketDto>(BasketRoutes.Basket + CRUDRouts.ReadAll + userName);
-    }
-
-    protected override async Task OnInitializedAsync()
-    {
-        var authstate = await _apiAuthenticationStateProvider.GetAuthenticationStateAsync();
-        userName = authstate.User.Identity.Name ?? "";
+        model = await _httpService.GetValueList<BasketDto>(BasketRoutes.Basket + CRUDRouts.ReadAll + $"/{userName}");
     }
 }
