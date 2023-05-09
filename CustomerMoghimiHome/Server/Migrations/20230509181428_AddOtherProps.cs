fe3d188dd92afcb8409ef568df2512b0a0de69e5
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-09 11:16:40 -0700

    add new mig for new props

---
+++ CustomerMoghimiHome/Server/Migrations/20230509181428_AddOtherProps.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerMoghimiHome.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOtherProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                schema: "dbo",
+                table: "CustomerDetailEntity",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                schema: "dbo",
+                table: "CustomerDetailEntity");
+        }
+    }
+}
