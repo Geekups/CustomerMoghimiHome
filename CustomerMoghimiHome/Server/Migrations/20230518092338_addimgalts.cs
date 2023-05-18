093781d35cf0edecb905268bfb357f75b0942c46
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-18 02:39:52 -0700

    Done

---
+++ CustomerMoghimiHome/Server/Migrations/20230518092338_addimgalts.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerMoghimiHome.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addimgalts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageAlt",
+                schema: "dbo",
+                table: "ProductEntity",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageAlt",
+                schema: "dbo",
+                table: "ProductCategoryEntity",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageAlt",
+                schema: "dbo",
+                table: "ProductEntity");
+
+            migrationBuilder.DropColumn(
+                name: "ImageAlt",
+                schema: "dbo",
+                table: "ProductCategoryEntity");
+        }
+    }
+}
