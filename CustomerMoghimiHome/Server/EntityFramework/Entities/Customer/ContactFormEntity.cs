d9de6b9025094d28c0682af316fdac246fcd910b
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-26 09:03:53 -0700

    add entity, fix decimal bug in price

---
+++ CustomerMoghimiHome/Server/EntityFramework/Entities/Customer/ContactFormEntity.cs
@@ -0,0 +1,28 @@
+﻿using CustomerMoghimiHome.Server.EntityFramework.Common;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerMoghimiHome.Server.EntityFramework.Entities.Customer;
+
+public class ContactFormEntity: BaseEntity
+{
+    public string UserId { get; set; }
+    public string Subject { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Massage { get; set; }
+}
+public class ContactFormEntityConfiguration : IEntityTypeConfiguration<ContactFormEntity>
+{
+    public void Configure(EntityTypeBuilder<ContactFormEntity> builder)
+    {
+        #region Properties features
+
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.UserId).IsRequired();
+        builder.Property(e => e.Email).IsRequired();
+        builder.Property(e => e.Massage).IsRequired();
+
+        #endregion
+    }
+}
