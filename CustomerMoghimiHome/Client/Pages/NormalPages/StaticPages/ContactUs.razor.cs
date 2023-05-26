1c712bb80f09bd427ff40351efcd9b20afda0652
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-26 10:12:22 -0700

    Contact Us plz

--- CustomerMoghimiHome/Client/Pages/NormalPages/StaticPages/ContactUs.razor.cs
+++ CustomerMoghimiHome/Client/Pages/NormalPages/StaticPages/ContactUs.razor.cs
@@ -1,4 +1,5 @@
-﻿using CustomerMoghimiHome.Shared.EntityFramework.DTO.Customer;
+﻿using CustomerMoghimiHome.Shared.Basic.Classes;
+using CustomerMoghimiHome.Shared.EntityFramework.DTO.Customer;
 
 namespace CustomerMoghimiHome.Client.Pages.NormalPages.StaticPages;
 
@@ -6,5 +7,12 @@ public partial class ContactUs
 {
 	#region Pre-Load
 	ContactFormDto model = new();
+	#endregion
+
+	#region Actions
+	public async Task Add()
+	{
+		using var response = await _httpService.PostValue(CustomerRoute.ContactForm + CRUDRouts.Create, model);
+	}
 	#endregion
 }
